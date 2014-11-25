18abd2f4948a7859973bbbc0844b20828ffc81a5
obb12 <omar.bb@hotmail.fi>
2014-11-25 13:54:36 +0200

    hello

--- .build/Result Resource Layer/js/main.js
+++ .build/Result Resource Layer/js/main.js
@@ -1,30 +1,38 @@
 
 window.onload = function () {
     // TODO:: Do your initialization job
+	$('button').show();
 
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
     });
-
-    // Sample code
-    $('.contents').click( function(){
-    	console.log("Sending ajax");
-    	setInterval(function() {
-    		sendAJAX("http://labs.humanisti.fixme.fi/gear-http/upload/wearable.txt", {}, function(response){
-    			var response = $.parseJSON( response );
-    			console.log(response[0]);
-    			
-    			
-    		
-    	    		if(response[0]===true)
-    	    		{
-    	    			navigator.vibrate(2000);
-    	    		}
-    	}, 100000);
-    	
-    	});
-    });
     
+    var load = function( numero ) {
+    	console.log( numero  + " painettu!");
+    	$('button').hide();
+    	setInterval(function() {
+			sendAJAX("http://labs.humanisti.fixme.fi/gear-http/upload/wearable.txt", {}, function(response){
+				var response = $.parseJSON( response );
+				console.log(response[ numero ]);
+				
+				if(response[ numero ]===true) {
+					navigator.vibrate(100);
+					$('body').html('Somebody commented your status!');
+				}
+		    } );
+    	}, 1000);
+	};
+    
+    // Sample code
+    $('#1').click( function(){
+    	load(0);
+    });
+    $('#2').click( function(){
+    	load(1);
+    });
+    $('#3').click( function(){
+    	load(2);
+    });
 };
